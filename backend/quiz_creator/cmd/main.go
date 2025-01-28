8505a1879d270467c452d2e4539c68c45fcb79bb
domesday <m.andriamiraho@gmail.com>
2025-01-28 15:37:30 +0100

    feat: db and start create quiz

--- backend/quiz_creator/cmd/main.go
+++ backend/quiz_creator/cmd/main.go
@@ -2,11 +2,20 @@ package quiz_creator
 
 import "github.com/gofiber/fiber/v2"
 
+type QuizCreator struct {
+}
+type QuizCreatorAI struct {
+}
+
 func main() {
 
 	app := fiber.New()
-	app.Get("/create-quiz", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	// Create a new Quiz
+	app.Post("/quiz")
+	// Create a new AI Quiz
+	app.Post("/quiz/ai", func(c *fiber.Ctx) error {
+		return c.SendString("Create AI quiz")
 	})
-	app.Listen(":3000")
+
+	app.Listen("0.0.0.0:3000")
 }
--- backend/quiz_creator/internal/handler/handle_create_quiz.go
+++ backend/quiz_creator/internal/handler/handle_create_quiz.go
@@ -1,5 +1,51 @@
-package handler
+package QuizzCreator
 
-func HandleCreateQuiz() {
+import "errors"
+
+type Answer struct {
+	Answer    string
+	IsCorrect bool
+}
+
+type Question struct {
+	Question string
+	Answer   []Answer
+}
+
+type Quiz struct {
+	QuizName  string
+	Questions []Question
+}
+
+type QuizCreator struct {
+	quiz Quiz
+}
+
+func (q QuizCreator) HandleCreateQuiz(quiz Quiz) {
+	// verify the content of the quiz
+	quiz.VerifyQuiz()
+
+	//
+}
+
+func (q QuizCreator) VerifyQuiz() {
+	// verify the content of the quiz
+	// check if the quiz has a name
+	if q.quiz.QuizName == "" {
+		return errors.New("Quiz name is required")
+	}
+	// check if the quiz has questions and there is no empty question
+	if len(q.quiz.Questions) == 0 {
+		return errors.New("Quiz must have at least one question")
+	}
+	// check if the questions have answers and there is no empty answer
+	// check if the answers have a correct answer
+
+	// if the quiz is not valid, return an error
+	// if the quiz is valid, return the quiz
+
+}
+
+func CreateQuiz() {
 
 }
