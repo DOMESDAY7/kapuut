b107dbcd6408731a03b5e33d2fd70546204c6097
DOMESDAY <m.andriamiraho@gmail.com>
2025-03-22 15:54:23 +0100

    fix: localhost -> 127.0.0.1

--- backend/quiz-leaderboard/src/main.rs
+++ backend/quiz-leaderboard/src/main.rs
@@ -86,7 +86,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(cors)
             .service(get_leaderboard)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
